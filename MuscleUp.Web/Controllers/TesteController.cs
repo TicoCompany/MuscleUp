842a154f9faa83ae22daa4a115e3db3a61f2a152
Marcelo de Souza <marcelodesouzasj@gmail.com>
2025-06-16 14:02:49 -0300

    Commites para teste integralçao com springbot

---
+++ MuscleUp.Web/Controllers/TesteController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MuscleUp.Web.Controllers;
+[AllowAnonymous]
+public class TesteController : BaseController
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+}
--- MuscleUp.Web/wwwroot/js/conta/module.js
+++ MuscleUp.Web/wwwroot/js/conta/module.js
@@ -6,21 +6,37 @@
         };
 
         $scope.login = function () {
-            $rootScope.carregando = true; 
+            $rootScope.carregando = true;
 
-            $http.post('/api/Contas/Login', $scope.usuario)
+            $http.post('http://localhost:8080/auth/login', $scope.usuario)
                 .then(function (response) {
-                    if (!response.data.sucesso)
-                        $mensagem.error(`${response.data.mensagem}`);
-                    else {
-                        $mensagem.success(response.data.mensagem);
-                        location.href = "/Home"
-                    }
+                    $mensagem.success("Login realizado com sucesso!");
+                    var usuario = response.data;
+                    sessionStorage.setItem("usuarioId", usuario.id);
+                    sessionStorage.setItem("usuarioNome", usuario.name);
+
+                    location.href = "/Home";
                 })
                 .catch(function (error) {
-                    $mensagem.error("Erro ao fazer login");
+                    $mensagem.error("Erro ao fazer login!");
                 }).finally(function () {
-                    $rootScope.carregando = false; 
+                    $rootScope.carregando = false;
+                });
+        };
+    });
+
+    app.controller("CadastroController", function ($scope, $http, $mensagem, $rootScope) {
+        $scope.cadastrar = function () {
+            $rootScope.carregando = true;
+            $http.post('http://localhost:8080/auth/register', $scope.usuario)
+                .then(function (response) {
+                    $mensagem.success("Conta cadastrada com sucesso!");
+                    location.href = "/Conta/Login";
+                })
+                .catch(function (error) {
+                    $mensagem.error("Erro ao criar a conta!");
+                }).finally(function () {
+                    $rootScope.carregando = false;
                 });
         };
 
