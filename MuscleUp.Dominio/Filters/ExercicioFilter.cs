8c7a1ce19515469afec3b3c110152f785cfd6b9a
Marcelo <marcelodesouzadev@gmail.com>
2025-06-17 23:36:00 -0300

    Correção de erros

--- MuscleUp.Dominio/Filters/ExercicioFilter.cs
+++ MuscleUp.Dominio/Filters/ExercicioFilter.cs
@@ -4,6 +4,7 @@ namespace MuscleUp.Dominio.Filters;
 
 public sealed record ExercicioFilter : PaginationFilter
 {
+    public int? IdAcademia { get; set; }
     public DificuldadeDoExercicio? Dificuldade { get; set; }
     public GrupoMuscular? GrupoMuscular { get; set; }
 }
